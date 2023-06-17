e785ac6edeb286a0f04d08cfd9c2e03d83d1108a
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-06-18 00:58:16 +0900

    feat: 全レコードを取得できるように

--- src/database.ts
+++ src/database.ts
@@ -1,22 +1,27 @@
 import sqlite3 from 'better-sqlite3'
 
 export type UserHistoryRecord = {
-    user: string
-    server: string
-    unix: number
-    time: number
+    user: string,
+    server: string,
+    channel: string,
+    unix: number,
+    time: number,
 }
 
 export const db = sqlite3('database.sqlite3')
 
 export function initialize() {
-    db.prepare('CREATE TABLE IF NOT EXISTS history(user TEXT, server TEXT, unix INTEGER, time INTEGER)').run()
+    db.prepare('CREATE TABLE IF NOT EXISTS history(user TEXT, server TEXT, channel TEXT, unix INTEGER, time INTEGER)').run()
 }
 
-export function insertHistory(user: string, server: string, unix: number, time: number) {
-    db.prepare('INSERT INTO history VALUES (?, ?, ?, ?)').run(user, server, unix, time)
+export function insertHistory(user: string, server: string, channel: string, unix: number, time: number) {
+    db.prepare('INSERT INTO history VALUES (?, ?, ?, ?, ?)').run(user, server, channel, unix, time)
 }
 
 export function getUserHistory(user: string) {
     return db.prepare('SELECT * FROM history WHERE user = ?').all(user) as UserHistoryRecord[]
+}
+
+export function getGlobalHistory() {
+    return db.prepare('SELECT * FROM history').all() as UserHistoryRecord[]
 }
