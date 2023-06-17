1ab46ce1abb1057ce0f88692e9841d3b372b4bab
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-06-18 00:57:02 +0900

    move: stateManagerを移動

--- src/discord/manager/state.ts
+++ src/discord/manager/state.ts
@@ -1,15 +1,12 @@
 export type UserState = {
     joinTime: number,
     serverId: string,
+    channelId: string,
 }
 
 export type ServerState = {
     globalStartTime: number,
-    channels: Map<string, ChannelState>,
-}
-
-export type ChannelState = {
-    startTime: number,
+    channels: Map<string, number>,
 }
 
 export const userStates = new Map<string, UserState>()
