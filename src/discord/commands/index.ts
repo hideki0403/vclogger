4fe6630c8981c8b3a59e96270b2fdde0bbe7ceaa
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-06-18 01:03:56 +0900

    feat: statsコマンドを実装

--- src/discord/commands/index.ts
+++ src/discord/commands/index.ts
@@ -1,7 +1,9 @@
 import show from './show'
 import current from './current'
+import stats from './stats'
 
 export default [
     show,
-    current
+    current,
+    stats,
 ]
---
+++ src/discord/commands/stats.ts
@@ -0,0 +1,51 @@
+import { ApplicationCommandOptionType } from 'discord.js'
+import BaseCommand from '../builder/command'
+import * as database from '@/database'
+import * as utils from '@/utils'
+
+export default new BaseCommand({
+    name: 'show',
+    description: '全ユーザーの通話の統計を表示します',
+    options: [{
+        name: 'only_this_server',
+        description: 'このサーバーでの通話履歴の統計を表示します',
+        type: ApplicationCommandOptionType.Boolean,
+        required: false
+    }],
+    execute: async (interaction) => {
+        const globalHistory = database.getGlobalHistory()
+        const onlyThisServer = interaction.inGuild() ? interaction.options.getBoolean('only_this_server') ?? false : false
+        const statistics = utils.calcurateStatistics(globalHistory, onlyThisServer ? interaction.guild!.id : undefined)
+
+        // VCに通話したことがなければ終了
+        if (!statistics.vcJoinCount) {
+            return interaction.reply(`通話履歴がありませんでした`)
+        }
+
+        interaction.reply({
+            embeds: [{
+                color: 0x88C0D0,
+                timestamp: new Date().toISOString(),
+                footer: {
+                    text: `記録開始日: ${statistics.firstJoinDate}`
+                },
+                author: {
+                    name: `${onlyThisServer ? interaction.guild?.name : '全サーバー'}の統計`,
+                    icon_url: interaction.guild?.iconURL() ?? undefined
+                },
+                description: `
+                    ### :speaker: 通話時間
+                    今日の通話時間: **${statistics.today}**
+                    過去一週間の合計通話時間: **${statistics.weekly}**
+                    過去一ヶ月間の合計通話時間: **${statistics.monthly}**
+
+                    ### :bar_chart: 統計
+                    累計通話時間: **${statistics.total}**
+                    平均通話時間: **${statistics.average}**
+                    最長記録: **${statistics.longest} (${statistics.longestTimeDate})**
+                    全ユーザーの通話した回数: **${statistics.vcJoinCount}回**
+                `.replace(/    /g, '')
+            }]
+        })
+    }
+})
